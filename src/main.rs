1f02108a209e1bf75d8a2e36d5000848f1a102f8
DeadAirX <djdivinity7@gmail.com>
2022-08-17 21:45:59 -0500

    🔴 Failing tests for bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+}
+
+fn bubble_sort(arr: Vec<i32>) -> Vec<i32> {
+    arr
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_bubble_sort_1() {
+        let arr: Vec<i32> = vec!(3, 5, 1, 2, 4);
+        let expected_sorted: Vec<i32> = vec!(1, 2, 3, 4, 5);
+
+        let actual_sorted = crate::bubble_sort(arr);
+
+        assert_eq!(expected_sorted, actual_sorted);
+    }
+
+    #[test]
+    fn test_bubble_sort_2() {
+        let arr: Vec<i32> = vec!(9, 7, 2, 20, 420, 69);
+        let expected_sorted: Vec<i32> = vec!(2, 7, 9, 20, 69, 420);
+
+        let actual_sorted = crate::bubble_sort(arr);
+
+        assert_eq!(expected_sorted, actual_sorted);
+    }
 }
