ad54d852d82b667034f02981c11434e9ffbfc6f0
DeadAirX <djdivinity7@gmail.com>
2022-08-19 22:48:43 -0500

    correct assertions

--- src/linear_search_test.rs
+++ src/linear_search_test.rs
@@ -5,10 +5,10 @@ mod linear_search_test {
         let arr = vec!(1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420);
 
         assert_eq!(crate::linear_search::linear_search(&arr, 69), true);
-        assert_eq!(crate::linear_search::linear_search(&arr, 1336), true);
+        assert_eq!(crate::linear_search::linear_search(&arr, 1336), false);
         assert_eq!(crate::linear_search::linear_search(&arr, 69420), true);
-        assert_eq!(crate::linear_search::linear_search(&arr, 69421), true);
+        assert_eq!(crate::linear_search::linear_search(&arr, 69421), false);
         assert_eq!(crate::linear_search::linear_search(&arr, 1), true);
-        assert_eq!(crate::linear_search::linear_search(&arr, 0), true);
+        assert_eq!(crate::linear_search::linear_search(&arr, 0), false);
     }
 }
